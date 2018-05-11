ba2e71abf6078d8136f1c81f437721589baf2b35
Michael Abeyta <mabeyta1990@gmail.com>
2018-05-11 17:52:25 -0600

    save

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "Jose103115!",
+        database: "burgers_db",
+    });
+};
---
+++ controllers/burgercontroller.js
@@ -0,0 +1,42 @@
+// NODE DEPENDENCIES & IMPORT burger.js
+var express = require('express');
+var burger = require('../models/burger.js');
+
+// CREATE ROUTER FOR APP TO BE EXPORTED 
+var router = express.Router();
+
+//CREATE ROUTES & LOGIC 
+
+router.get('/', function(req, res) {
+  
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+            };
+    // console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+ 
+router.post('/burgers', function(req, res) {
+  burger.insertOne([
+    'burger_name'
+  ], [
+    req.body.burger_name
+  ], function(data) {
+    res.redirect('/');
+  });
+});
+
+router.put('/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.updateOne({
+    devoured: true
+  }, condition, function(data) {
+    res.redirect('/');
+  });
+});
+
+// EXPORT ROUTES
+module.exports = router;
